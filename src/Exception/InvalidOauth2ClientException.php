df026ecb2d778b7e093521da80cca42bdcfd434b
Jay Friendly <git@jaypan.com>
2019-01-07 09:22:42 +1100

    Initial file commit of 8.x-2.x version. Re-writes entire module to use PHP OAuth 2.0 Client library for OAuth2 handling

---
+++ src/Exception/InvalidOauth2ClientException.php
@@ -0,0 +1,36 @@
+<?php
+
+use Exception;
+use Throwable;
+
+/**
+ * Exception thrown when trying to retrieve a non-existent OAuth2 Client.
+ */
+class InvalidOauth2ClientException extends Exception {
+
+  /**
+   * Constructs an InvalidOauth2ClientException object.
+   *
+   * @param string $invalidClientId
+   *   The passed Oauth2 Client ID that was found to be invalid.
+   * @param string $message
+   *   The Exception message to throw.
+   * @param int $code
+   *   The Exception code.
+   * @param \Throwable $previous
+   *   The previous exception used for the exception chaining.
+   */
+  public function __construct($invalidClientId, $message = "", $code = 0, Throwable $previous = NULL) {
+    if ($message == "") {
+      if (is_scalar($invalidClientId)) {
+        $message = "The OAuth2 CLient plugin ''"$plugin_id . "' does not exist";
+      }
+      else {
+        $message = "An invalid value was passed for the OAuth2 Plugin ID";
+      }
+    }
+
+    parent::__construct($message, $code, $previous);
+  }
+
+}
---
+++ src/Service/Oauth2ClientServiceBase.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\oauth2_client\Service;
+
+/**
+ * Base class for OAuth2 Client services.
+ */
+abstract class Oauth2ClientServiceBase implements Oauth2ClientServiceInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getClient($clientId) {
+    $clients = &drupal_static(__CLASS__ . '::' . __FUNCTION__, []);
+    if (!isset($clients[$clientId])) {
+      $definition = $this->oauth2ClientPluginManager->getDefinition($clientId);
+      if (!$definition || !isset($definition['id'])) {
+        throw new InvalidOauth2ClientException($client);
+      }
+
+      $clients[$clientId] = $this->oauth2ClientPluginManager->createInstance($definition['id']);
+    }
+
+    return $clients[$clientId];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function retrieveAccessToken($clientId) {
+    return $this->state->get('oauth2_client_access_token-' . $clientId);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function clearAccessToken($clientId) {
+    return $this->state->delete('oauth2_client_access_token-' . $clientId);
+  }
+
+}
